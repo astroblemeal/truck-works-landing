46ebbf8efae98bd9ea2fc8cd8e4c3f4695a94a32
Alexander <alexander@Alex.true.true>
2021-08-17 16:39:54 +0700

    okok

--- src/components/footer/footer.data.js
+++ src/components/footer/footer.data.js
@@ -1,42 +1,33 @@
 export const menuItems = [
   {
     id: 1,
-    title: 'Email Us:',
+    title: "Email Us:",
     items: [
       {
-        path: '#!',
-        label: 'accounts@truckworksltd.co.uk',
+        path: "#!",
+        label: "accounts@truckworksltd.co.uk",
       },
       {
-        path: '#!',
-        label:  'transport@truckworksltd.co.uk',
+        path: "#!",
+        label: "transport@truckworksltd.co.uk",
       },
       {
-        path: '#!',
-        label: 'office@truckworksltd.co.uk',
+        path: "#!",
+        label: "office@truckworksltd.co.uk",
       },
       {
-        path: '#!',
-        label: 'info@truckworksltd.co.uk',
+        path: "#!",
+        label: "info@truckworksltd.co.uk",
       },
-      {
-        path: '#!',
-        label: 'alan@truckworksltd.co.uk',
-      },
-      {
-        path: '#!',
-        label: 'chris@truckworksltd.co.uk',
-      },
-
     ],
   },
   {
     id: 2,
-    title: 'Phone Us:',
+    title: "Phone Us:",
     items: [
       {
-        path: '#!',
-        label: '07798947762',
+        path: "#!",
+        label: "07798947762",
       },
     ],
   },
@@ -44,8 +35,7 @@ export const menuItems = [
 
 export const footerNav = [
   {
-    path: '/',
-    label: 'Home',
+    path: "/",
+    label: "Home",
   },
-
 ];
