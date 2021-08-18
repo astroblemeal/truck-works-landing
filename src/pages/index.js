bfc67420704dcd6d02fa5c756d7a570635fa1abd
Alexander Eklund <alexek1987@icloud.com>
2021-08-19 02:36:59 +0700

    fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,30 +4,27 @@ import theme from 'theme';
 import SEO from 'components/seo';
 import Layout from 'components/layout';
 import Banner from 'sections/banner';
-import UltimateFeatures from 'sections/ultimate-features';
 import Features from 'sections/features';
-import IntroVideo from 'sections/intro-video';
-import UsefulFeatures from 'sections/useful-features';
 import Widgets from 'sections/widgets';
-import Pricing from 'sections/pricing';
-import Faq from 'sections/faq';
-<<<<<<< HEAD
 import Contact from 'sections/contact';
-=======
->>>>>>> 18621bbdf97522a24fc9c86be206a30c3754a57f
+// import UltimateFeatures from 'sections/ultimate-features';
+// import IntroVideo from 'sections/intro-video';
+// import UsefulFeatures from 'sections/useful-features';
+// import Pricing from 'sections/pricing';
+// import Faq from 'sections/faq';
+
 
 export default function IndexPage() {
   return (
     <ThemeProvider theme={theme}>
       <Layout>
-        <SEO title="Truck Works Ltd" />
+        <SEO title="Truck Works" />
         <Banner />
         <Widgets />
         <Features />
-<<<<<<< HEAD
+
         <Contact />
-=======
->>>>>>> 18621bbdf97522a24fc9c86be206a30c3754a57f
+
       </Layout>
     </ThemeProvider>
   );
