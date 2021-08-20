2cf6d51e5df7ed1f324c61102663ec1ddadf1981
Alexander Eklund <alexek1987@icloud.com>
2021-08-20 08:38:06 +0700

    ok

--- src/sections/contact.js
+++ src/sections/contact.js
@@ -1,83 +1,73 @@
-import React from "react";
-import { useState } from "react";
-import { TextField } from "@material-ui/core";
-import axios from "axios";
-import { emailValidator } from "../functions/validator";
-import {  Button,Text, heading } from 'theme-ui';
-import SectionHeading from 'components/section-heading';
-import { Fragment } from 'react';
-
-
-
+import React from "react"
+import { useState } from "react"
+import { TextField } from "@material-ui/core"
+import axios from "axios"
+import { emailValidator } from "../functions/validator"
+import { Button, Text, heading } from "theme-ui"
+import SectionHeading from "components/section-heading"
+import { Fragment } from "react"
 
 function Contact() {
-  const [name, setName] = useState("");
-  const [number, setNumber] = useState("");
-  const [mail, setMail] = useState("");
-  const [message, setMessage] = useState("");
+  const [name, setName] = useState("")
+  const [number, setNumber] = useState("")
+  const [mail, setMail] = useState("")
+  const [message, setMessage] = useState("")
 
   const submitHandler = (e) => {
-    e.preventDefault();
-    let isEmailVerified = emailValidator(mail);
+    e.preventDefault()
+    let isEmailVerified = emailValidator(mail)
     if (isEmailVerified === true) {
-      const tableData = { name, mail, number, message };
-    //   axios
-    //     .post(
-    //       "",
-    //       tableData
-    //     )
-    //     .then((response) => {
-    //       alert("Thank you our team will reach out to you as soon as possible.");
-    //     });
-     alert("Thank you our team will reach out to you as soon as possible.")
-      setName("");
-      setNumber("");
-      setMail("");
-      setMessage("");
+      const tableData = { name, mail, number, message }
+      //   axios
+      //     .post(
+      //       "",
+      //       tableData
+      //     )
+      //     .then((response) => {
+      //       alert("Thank you our team will reach out to you as soon as possible.");
+      //     });
+      alert("Thank you our team will reach out to you as soon as possible.")
+      setName("")
+      setNumber("")
+      setMail("")
+      setMessage("")
     } else {
-      alert("Enter valid email");
+      alert("Enter valid email")
     }
-  };
+  }
 
   return (
-  <Fragment>
-      <form style={formStyle}>
-         <SectionHeading
-              sx={headingStyles}
-              title={
-                <Fragment>
-                  Drop us a line!
-                </Fragment>
-              }
-              description="We usually respond within 24 business hours!"
-            />
-         
+    <Fragment>
+      <form onSubmit={submitHandler} style={formStyle}>
+        <SectionHeading
+          sx={headingStyles}
+          title={<Fragment>Drop us a line!</Fragment>}
+          description='We usually respond within 24 business hours'
+        />
+
         <TextField
           onChange={(e) => setName(e.target.value)}
           value={name}
-        style={textFieldStyle}
-       
+          style={textFieldStyle}
           label='Name'
-          variant="outlined"
+          variant='outlined'
         />
 
         <TextField
           required
           onChange={(e) => setMail(e.target.value)}
           value={mail}
-        style={textFieldStyle}
-       
-          label="Email"
-          variant="outlined"
+          style={textFieldStyle}
+          label='Email'
+          variant='outlined'
         />
 
         <TextField
           value={number}
           onChange={(e) => setNumber(e.target.value)}
-         style={textFieldStyle}
-      
+          style={textFieldStyle}
           label='Telephone number'
-          variant="outlined"
+          variant='outlined'
         />
         <TextField
           value={message}
@@ -86,52 +76,56 @@ function Contact() {
           label='Message'
           multiline
           rows={4}
-          variant="outlined"
+          variant='outlined'
         />
-        <Button onClick={submitHandler}  style={buttonStyle} >
-            <a style={{textDecoration: "none", color: "white"}} href="tel:07798947762">{mail.length === 0 ? "*Email " : "Send"} </a>
+
+        <Button type='submit' style={buttonStyle}>
+          <a
+            style={{ textDecoration: "none", color: "white" }}
+            href='tel:07798947762'>
+            {mail.length === 0 ? "*Email " : "Send"}{" "}
+          </a>
         </Button>
       </form>
-     </Fragment>
-  );
+    </Fragment>
+  )
 }
 
-export default Contact;
+export default Contact
 
 const buttonStyle = {
-    marginBottom: "10px",
-    width: "200px"
+  marginBottom: "10px",
+  width: "200px",
 }
 
 const textFieldStyle = {
-      marginBottom: "10px"
+  marginBottom: "10px",
 }
 
 const headingStyles = {
-
-    background: "white",
-    textAlign: 'left',
-    mb: ['20px'],
-    mt: [0, 0, '-70px'],
-    h2: {
-      fontSize: ['28px', '28px', '28px', '28px', '36px', '40px'],
-      lineHeight: [1.25, 1.5],
-      letterSpacing: '-1.5px',
-      br: {
-        display: ['none', 'none', 'none', 'block'],
-      },
+  background: "white",
+  textAlign: "left",
+  mb: ["20px"],
+  mt: [0, 0, "-70px"],
+  h2: {
+    fontSize: ["28px", "28px", "28px", "28px", "36px", "40px"],
+    lineHeight: [1.25, 1.5],
+    letterSpacing: "-1.5px",
+    br: {
+      display: ["none", "none", "none", "block"],
     },
-    p: {
-      mt: ['15px', '10px'],
-    },
-  }
+  },
+  p: {
+    mt: ["15px", "10px"],
+  },
+}
 
-  const formStyle = {
-      display: "flex", 
-      flexDirection: "column", 
-      background: "white", 
-      paddingRight: "200px", 
-      paddingLeft: "200px", 
-      paddingTop: "90px", 
-      paddingBottom: "40px"
-  }
+const formStyle = {
+  display: "flex",
+  flexDirection: "column",
+  background: "white",
+  paddingRight: "200px",
+  paddingLeft: "200px",
+  paddingTop: "90px",
+  paddingBottom: "40px",
+}
